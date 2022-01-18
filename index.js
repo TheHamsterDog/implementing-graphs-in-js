48372838d0c4b2b423fa7952a5fade71af27bdf3
Shreshth Verma <shresth21oct@gmail.com>
2022-01-18 09:53:43 +0000

    completed graph

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-class Graph { 
-  constructor() { 
+class Graph {
+  constructor() {
     this.numberOfNodes = 0;
     this.adjacentList = {
-    }; 
-  } 
-  addVertex(node)  { 
-  } 
-  addEdge(node1, node2) { 
+    };
+  }
+  addVertex(node) {
+    if (this.adjacentList[node]) {
+      return this.adjacentList[node];
+    }
+    this.numberOfNodes++;
+    this.adjacentList[node] = [];
+    return this.adjacentList[node]
+  }
+  addEdge(node1, node2) {
+    console.log(this.numberOfNodes)
+    if (this.adjacentList[node1].includes(node2)) {
+      return;
+    }
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
     //undirected Graph 
-  } 
-  showConnections() { 
-    const allNodes = Object.keys(this.adjacentList); 
-    for (let node of allNodes) { 
-      let nodeConnections = this.adjacentList[node]; 
-      let connections = ""; 
+  }
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
       let vertex;
       for (vertex of nodeConnections) {
         connections += vertex + " ";
-      } 
-      console.log(node + "-->" + connections); 
-    } 
-} 
-} 
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+}
 
 const myGraph = new Graph();
 myGraph.addVertex('0');
@@ -31,16 +43,16 @@ myGraph.addVertex('3');
 myGraph.addVertex('4');
 myGraph.addVertex('5');
 myGraph.addVertex('6');
-myGraph.addEdge('3', '1'); 
-myGraph.addEdge('3', '4'); 
-myGraph.addEdge('4', '2'); 
-myGraph.addEdge('4', '5'); 
-myGraph.addEdge('1', '2'); 
-myGraph.addEdge('1', '0'); 
-myGraph.addEdge('0', '2'); 
+myGraph.addEdge('3', '1');
+myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
 myGraph.addEdge('6', '5');
 
-myGraph.showConnections(); 
+myGraph.showConnections();
 //Answer:
 // 0-->1 2 
 // 1-->3 2 0 
