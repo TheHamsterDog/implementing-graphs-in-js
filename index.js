3c9ab2133a1bc2fe847f9aef0fbff0b0fe0aa083
Shreshth Verma <shresth21oct@gmail.com>
2022-01-18 10:06:05 +0000

    removed unnecessary comments

--- index.js
+++ index.js
@@ -70,11 +70,3 @@ myGraph.removeEdge('6', '5');
 myGraph.showConnections()
 myGraph.removeVertex("6")
 myGraph.showConnections()
-//Answer:
-// 0-->1 2 
-// 1-->3 2 0 
-// 2-->4 1 0 
-// 3-->1 4 
-// 4-->3 2 5 
-// 5-->4 6 
-// 6-->5
